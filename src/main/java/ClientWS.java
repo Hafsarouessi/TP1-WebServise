e63fc9a45eb13c1acde30dc1f8a3dd3c62995846
[hafsa] <[hafsa@gmail.com]>
2024-11-10 12:56:13 +0100

    second commit

--- src/main/java/ClientWS.java
+++ src/main/java/ClientWS.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import mypackage.BanqueService;
+import mypackage.BanqueWS;
+import mypackage.Compte;
 
-public class Client {
+public class ClientWS {
+    public static void main(String[] args) {
+        BanqueService stud=new BanqueWS().getBanqueServicePort();
+        System.out.println(stud.convert(7600));
+        Compte cp=stud.getCompte(5);
+        System.out.println(cp.getCode());
+        System.out.println(cp.getSolde());
+    }
 }
